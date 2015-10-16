d05cc80a815f855dbcb6a091715004d8f68adb1d
David Liu <info@mrdavidliu.me>
2015-10-16 02:56:48 -0400

    Bug Fixes

--- app/src/main/java/co/mrdavidliu/uwinfosession/MainActivity.java
+++ app/src/main/java/co/mrdavidliu/uwinfosession/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.roomorama.caldroid.CaldroidFragment;
 
 import org.json.JSONArray;
 import org.json.JSONException;
